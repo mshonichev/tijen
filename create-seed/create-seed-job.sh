ceb4ab18a021f7515995267cdb723bb248fc5c3d
mshonichev <mshonichev@gridgain.com>
2018-06-28 19:47:49 +0300

    test jobDSL

--- create-seed/create-seed-job.sh
+++ create-seed/create-seed-job.sh
@@ -4,4 +4,4 @@ cd $(dirname $0)
 
 jenkins-jobs --conf jenkins.ini update job-generator.yaml
 
-cat job-generator.yaml | yq -r '.[].job.name' | xargs -i{} echo "*** Updated {} job ***"
+#cat job-generator.yaml | yq -r '.[].job.name' | xargs -i{} echo "*** Updated {} job ***"
