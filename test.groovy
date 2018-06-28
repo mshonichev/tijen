add7945f9253c2eef97e8e314fd5bba149ead62d
mshonichev <mshonichev@gridgain.com>
2018-06-28 20:05:24 +0300

    test jobDSL

--- test.groovy
+++ test.groovy
@@ -4,5 +4,12 @@ folder(ignite_version) {
     displayName('Release ' + ignite_version)
     description('Distributed tests for release ' + ignite_version)
 
-    
+    job('prepare') {
+        parameters {
+            string(name='IGNITE_VERSION', defaultValue=ignite_version)
+        }
+        steps {
+             shell('set')
+        }
+    }
 }
