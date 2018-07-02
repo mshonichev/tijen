db73d7a9709f97c0a3effcd5a21fcba16b8bce93
mshonichev <mshonichev@gridgain.com>
2018-07-02 15:15:51 +0300

    add more jobs to release-jobs.2.5.1.yaml job definition now respect 'option', 'attr', 'attr_match' `job-tiden-debug` now autogenerated from `job-tiden` by removing ftp publishers to 'Publish_to_QA_FTP' site.

--- prepare-gg-release-tasks.py
+++ prepare-gg-release-tasks.py
@@ -1,6 +1,7 @@
 import os
 from util import versioned_yaml, load_yaml, save_yaml, camelcase
 from optparse import OptionParser
+from copy import deepcopy
 
 if __name__ == "__main__":
     parser = OptionParser()
@@ -45,6 +46,26 @@ if __name__ == "__main__":
     res_template = os.path.abspath(os.path.join(options.res_dir, 'template.yaml'))
     print("*** Loading %s ***" % res_template)
     template.extend(load_yaml(res_template))
+
+    tiden_job = None
+    for k, v in enumerate(template):
+        if 'job-template' in template[k]:
+            if 'id' in template[k]['job-template']:
+                if 'job-tiden' == template[k]['job-template']['id']:
+                    tiden_job = template[k].copy()
+                    break
+
+    if tiden_job:
+        tiden_debug_job = deepcopy(tiden_job)
+        tiden_debug_job['job-template']['id'] = 'job-tiden-debug'
+        for k,v in enumerate(tiden_debug_job['job-template']['publishers']):
+            if 'ftp' in tiden_debug_job['job-template']['publishers'][k]:
+                if 'Publish_to_QA_FTP' == tiden_debug_job['job-template']['publishers'][k]['ftp']['site']:
+                    del tiden_debug_job['job-template']['publishers'][k]
+
+                    break
+
+        template.append(tiden_debug_job)
 
     print("*** Loading jobs specifications from %s ***" % os.path.abspath(options.res_dir))
     jobs = versioned_yaml(ignite_version, test_plan + '-jobs.*.yaml', os.path.abspath(options.res_dir))
